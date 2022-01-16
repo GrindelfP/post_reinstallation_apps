60634ec4d24f9ddf837541f98ec4634bd281c54e
GrindelfP <grindelf.perlomutrovij@gmail.com>
2022-01-16 22:27:24 +0300

    initial_2

---
+++ post_inst_app_scrpt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install gimp python3.9 git-all -y
+#sudo apt install ./zoom_amd64.deb -y
+sudo snap install telegram-desktop vlc spotify youtube-music-desktop-app kdenlive
+sudo snap install slack --classic
+
