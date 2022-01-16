203cd45f556e0067e3922c5ee92cb1bb0c8922ae
GrindelfP <grindelf.perlomutrovij@gmail.com>
2022-01-16 23:05:34 +0300

    add chrome

--- post_inst_app_scrpt.sh
+++ post_inst_app_scrpt.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+#installing applications
 sudo apt install gimp python3.9 git-all -y
-#sudo apt install ./zoom_amd64.deb -y
 sudo snap install telegram-desktop vlc spotify youtube-music-desktop-app kdenlive
 sudo snap install slack --classic
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
+#deleting unuseful things
+sudo rm google-chrome-stable_current_amd64.deb
+
+#TODO: JetBrains Toolbox, ZOOM
