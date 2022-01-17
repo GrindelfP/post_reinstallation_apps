bd705f9ab930a8b519607afcc2a6cc0d66d69a18
GrindelfP <grindelf.perlomutrovij@gmail.com>
2022-01-17 22:07:56 +0300

    add zoom & jetbrains toolbox

--- post_inst_app_scrpt.sh
+++ post_inst_app_scrpt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #installing applications
-sudo apt install gimp python3.9 git-all -y
-sudo snap install telegram-desktop vlc spotify youtube-music-desktop-app kdenlive
+sudo apt install gimp python3.9 git-all curl -y
+sudo snap install telegram-desktop vlc spotify youtube-music-desktop-app kdenlive zoom-client
 sudo snap install slack --classic
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+curl https://gist.githubusercontent.com/greeflas/431bc50c23532eee8a7d6c1d603f3921/raw | bash
 
 #deleting unuseful things
 sudo rm google-chrome-stable_current_amd64.deb
 
-#TODO: JetBrains Toolbox, ZOOM
