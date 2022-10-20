adcdce72e34fee673e8c7669768981fafcb35bf7
Grindelf <63808275+GrindelfP@users.noreply.github.com>
2022-10-20 15:30:16 +0300

    Update post_inst_app_scrpt.sh

--- post_inst_app_scrpt.sh
+++ post_inst_app_scrpt.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #installing applications
 sudo apt install gimp python3.9 git-all curl python3-pip -y
-sudo snap install spotify youtube-music-desktop-app kdenlive zoom-client
-sudo snap install slack --classic
+sudo snap install kdenlive
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
 curl https://gist.githubusercontent.com/greeflas/431bc50c23532eee8a7d6c1d603f3921/raw | bash
